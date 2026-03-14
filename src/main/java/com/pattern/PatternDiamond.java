d9eb4b8bc0d7bc37db0f07c1e2dcac20968e8bdd
pranay-atkari <pranayatkari58@gmail.com>
2026-03-14 15:05:18 +0530

    removed redundant variable and improved clarity

--- src/main/java/com/pattern/PatternDiamond.java
+++ src/main/java/com/pattern/PatternDiamond.java
@@ -2,44 +2,34 @@ package com.pattern;
 
 public class PatternDiamond {
 
-
     private static final int PATTERN_SIZE = 5;
 
     public static void main(String[] args) {
 
-        int n = PATTERN_SIZE;
-
-
-        for (int i = 1; i <= n; i++) {
-            printRow(n, i);
+        for (int i = 1; i <= PATTERN_SIZE; i++) {
+            printRow(PATTERN_SIZE, i);
         }
 
-
-        for (int i = n - 1; i >= 1; i--) {
-            printRow(n, i);
+        for (int i = PATTERN_SIZE - 1; i >= 1; i--) {
+            printRow(PATTERN_SIZE, i);
         }
     }
 
+    private static void printRow(int size, int row) {
 
-    private static void printRow(int n, int i) {
-
-
-        for (int j = i; j < n; j++) {
+        for (int j = row; j < size; j++) {
             System.out.print(" ");
         }
 
-
         System.out.print("*");
-
-
-        if (i > 1) {
-            for (int j = 1; j <= (2 * i - 3); j++) {
+ 
+        if (row > 1) {
+            for (int j = 1; j <= (2 * row - 3); j++) {
                 System.out.print(" ");
             }
             System.out.print("*");
         }
 
-     
         System.out.println();
     }
 }
