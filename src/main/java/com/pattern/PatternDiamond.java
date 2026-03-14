1b5bb47dafb01089716853d5868f193afaf2af71
pranay-atkari <pranayatkari58@gmail.com>
2026-03-14 14:59:10 +0530

    removed magic number and duplicate logic

--- src/main/java/com/pattern/PatternDiamond.java
+++ src/main/java/com/pattern/PatternDiamond.java
@@ -1,47 +1,45 @@
 package com.pattern;
 
-
-
 public class PatternDiamond {
+
+
+    private static final int PATTERN_SIZE = 5;
 
     public static void main(String[] args) {
 
-        int n = 5;
+        int n = PATTERN_SIZE;
+
 
         for (int i = 1; i <= n; i++) {
-
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
-            }
-
-            System.out.print("*");
-
-            if (i > 1) {
-                for (int j = 1; j <= (2 * i - 3); j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-            }
-
-            System.out.println();
+            printRow(n, i);
         }
+
 
         for (int i = n - 1; i >= 1; i--) {
+            printRow(n, i);
+        }
+    }
 
-            for (int j = n; j > i; j--) {
+
+    private static void printRow(int n, int i) {
+
+
+        for (int j = i; j < n; j++) {
+            System.out.print(" ");
+        }
+
+
+        System.out.print("*");
+
+
+        if (i > 1) {
+            for (int j = 1; j <= (2 * i - 3); j++) {
                 System.out.print(" ");
             }
-
             System.out.print("*");
-
-            if (i > 1) {
-                for (int j = 1; j <= (2 * i - 3); j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-            }
-
-            System.out.println();
         }
+
+     
+        System.out.println();
     }
 }
